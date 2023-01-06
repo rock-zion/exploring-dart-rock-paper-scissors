ab114da830d9ee94cb80139612854f8f73571023
rockxzion <rock_zion@outlook.com>
2023-01-07 00:38:34 +0100

    create pseudocode for app

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -1,3 +1,15 @@
+/**
+ * Show prompt
+ * Read user input from console
+ * If input is s valid move ("r","p","s");
+ * Choose the AI move at random
+ * Compare the player move with the AI move
+ * Show the result
+ * else if input is q,
+ * Quit the program
+ * else Invalid input
+ */
+
 void main() {
   print('Hello world!');
 }
