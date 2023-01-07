5fc944df194b03cfe559deed2b2ed951468f9d87
rockxzion <rock_zion@outlook.com>
2023-01-07 01:10:50 +0100

    make decision based on user input, decide to keep playing or quit the game

--- rock_paper_scissors.dart
+++ rock_paper_scissors.dart
@@ -12,10 +12,18 @@
 
 import 'dart:io';
 
+
 void main() {
   while (true) {
     stdout.write('Rock, paper or scissors? (r/p/s) ');
     final input = stdin.readLineSync();
-    print('You typed $input');
+    if (input == "r" || input == "p" || input == "s") {
+      print('Selected $input');
+    } else if (input == "q") {
+      print('Come back next time');
+      break;
+    } else {
+      print('Invalid input, try again');
+    }
   }
 }
